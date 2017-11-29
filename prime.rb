bf94afabecc4cd3f1432cc88aa82c4c15a603648
Mark Bello <mcbello51286@gmail.com>
2017-11-29 03:21:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if number < 0 || number == 1 || number == 0
+    return false
+  end
+  range = (2...number).to_a
+  range.each{
+    |x|
+    if number % x == 0
+      return false
+    end
+  }
+  true
+end
