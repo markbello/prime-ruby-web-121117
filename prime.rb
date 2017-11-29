da91f4f2249d4c0ca21b626d0fa426044528a77c
Mark Bello <mcbello51286@gmail.com>
2017-11-29 03:10:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  number > 0 ? range = [2..number] : false
+  number > 0 ? range = [2..number] : return false
   range.each{
     |x|
     if number % x == 0 || number < 0
